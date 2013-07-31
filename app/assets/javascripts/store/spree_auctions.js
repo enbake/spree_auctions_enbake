42e5a835eaa05d757d81a7413afb27a721db7e5d
puneet <pgupta@enbake.com>
2013-07-31 16:26:41 +0530

    Fixed js issue

--- app/assets/javascripts/store/spree_auctions.js
+++ app/assets/javascripts/store/spree_auctions.js
@@ -1,26 +1,12 @@
 //= require store/spree_frontend
 
-$(document).ready(function(){
-	$('#add-to-cart-button').attr('disabled', true)
-})
-
-$(document).on('blur', '#bid_price', function(){
-	var min_bid = $('#bd_pz').text().slice(2)
-	var user_bid = $(this).val().trim()
-	if((parseFloat(user_bid) > parseFloat(min_bid)) && user_bid != ""){
-		$('#add-to-cart-button').attr('disabled', false);
-	}else{
-		$( "#dialog" ).dialog();
-		$('#add-to-cart-button').attr('disabled', true);
-	}
-})
-
-$(document).on('click', '.ui-button', function(){
-	$('#add-to-cart-button').attr('disabled', true);
-})
-
 $(document).on('click', '#add-to-cart-button', function(){
-	if($('#bid_price').val()==""){
-		$('#add-to-cart-button').attr('disabled', true);
+	var min_bid = $('#bd_pz').text().slice(2)
+	var user_bid = $('#bid_price').val().trim()
+	if((parseFloat(user_bid) <= parseFloat(min_bid)) || user_bid == ""){
+		$( "#dialog" ).dialog();
+	}else{
+		$(this).attr('type','submit');
+		$(this).trigger('click');
 	}
 })
