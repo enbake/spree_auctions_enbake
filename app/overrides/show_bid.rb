8dc040915e564de4a883b8ae73453ab66fa693cc
Oskar Zabik <oskar.zabik@gmail.com>
2013-12-27 14:50:05 +0100

    improving product page

--- app/overrides/show_bid.rb
+++ app/overrides/show_bid.rb
@@ -1,9 +1,9 @@
-Deface::Override.new(:virtual_path  => "spree/products/_cart_form", :replace => "div#product-price", :text  => '<div id="product-price" style="float:none">
+Deface::Override.new(:virtual_path  => "spree/products/_cart_form", :replace => "div#product-price", :text  => '<div id="product-price">
           <h6><%= Spree.t(:minimum_bid) %></h6>
           <div>
-            <span class="price selling" id="bd_pz" itemprop="price">
+            <h1 class="price selling" id="bd_pz" itemprop="price">
               <%= display_price(@product.master) %>
-            </span>
+            </h1>
             <span itemprop="priceCurrency" content="<%= @product.currency %>"></span>
           </div>
 
