92bde11ceca488821e4057a167edb830bbed1aa0
vishal <vsharma@enbake.com>
2014-07-07 14:55:52 +0530

    Added Migration for Catalog.

---
+++ db/migrate/20140707074624_add_catalog_to_navigation.rb
@@ -0,0 +1,13 @@
+class AddCatalogToNavigation < ActiveRecord::Migration
+  def change
+    Spree::Navigation.delete_all
+     Spree::Navigation.create(name: 'home', url: "/", private: 'false') 
+    Spree::Navigation.create(name: 'Catalog', url: "/catalog", private: 'false')
+    Spree::Navigation.create(name: 'Best_Offer', url: "#", private: 'false') 
+    Spree::Navigation.create(name: 'About_us', url: "#", private: 'false')
+    Spree::Navigation.create(name: 'Contact', url: "#", private: 'false') 
+    Spree::Navigation.create(name: 'profile', url: "/profile", private: 'true') 
+    Spree::Navigation.create(name: 'auctions', url: "/auctions", private: 'true') 
+    Spree::Navigation.create(name: 'payment', url: '/payment', private: 'true')
+  end
+end
---
+++ spec/models/spree/navigation_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Spree::Navigation do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
