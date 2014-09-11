02b42783120585bef1569bb01fb67062efc00a2a
Enbake <enbake@enbake-Aspire-E1-530.(none)>
2014-09-11 15:42:34 +0530

    Added User_registrations_controller_decorator file

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -1,10 +1,9 @@
 module Spree
-   class UserRegistrationsController < Devise::RegistrationsController
-   
+   UserRegistrationsController.class_eval do
      def create
         @user = build_resource(params[:spree_user])
         if resource.save
-          subscribe_user
+          subscribe_user if  @user.subscribed
           set_flash_message(:notice, :signed_up)
           sign_in(:spree_user, @user)
           session[:spree_user_signup] = true
@@ -16,19 +15,19 @@ module Spree
         end
       end
 
-    def subscribe_user
-      gb = Gibbon::API.new(AppConfig.mailchimp["merchant_id"])
-      list = gb.lists.list({:filters => {:list_name => "customer"}})["data"].first
-      unless list.nil?
-        email = @user.email
-        begin
-        result = gb.lists.subscribe({:id => list["id"], :email => {:email => email},
-                           :merge_vars => {:FNAME => "", :LNAME => ""},
-                           :double_optin => false})
-        rescue Gibbon::MailChimpError => e
-          flash[:error] = email + " already exists."
+    private
+
+      def subscribe_user
+        gb = Gibbon::API.new(AppConfig.mailchimp["merchant_id"])
+        chimp = gb.lists.list({:filters => {:list_name => "Tester"}})["data"].first
+        unless chimp.nil?
+          email = @user.email
+          begin
+          result = gb.lists.subscribe({:id => "5ca497ea43", :email => {:email => email}, :merge_vars => {:FNAME => "", :LNAME => ""}, :double_optin => false})
+          rescue Gibbon::MailChimpError => e
+            flash[:error] = email + " already exists."
+          end
         end
       end
-    end
    end
  end
--- app/overrides/alter_footer.rb
+++ app/overrides/alter_footer.rb
@@ -1,1 +1,1 @@
-Deface::Override.new(:virtual_path  => "spree/shared/_footer",:replace => "div#footer-left p", :text  => "<%= Spree.t :powered_by  %> <%= link_to 'Action House', '/' %>", :name => "alter_footer")
+Deface::Override.new(:virtual_path  => "spree/shared/_footer",:replace => "div#footer-left p", :text  => "<%= Spree.t :powered_by  %> <%= link_to 'Auction House', '/' %>", :name => "alter_footer")
