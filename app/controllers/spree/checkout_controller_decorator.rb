967d731ba6873d27170ce78f03cf1e82ef8804d9
Mayank <mmishra@enbake.com>
2014-10-14 12:28:15 +0530

    reverting the change of checkout process

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,29 +1,21 @@
 module Spree
   CheckoutController.class_eval do
     def update
-      #object_params["bill_address_attributes"]["state_name"] = "teststate"
       if @order.update_attributes(object_params)
-        if object_params.present?
-          @order.user.ship_address_id = @order.ship_address_id
-          @order.user.bill_address_id = @order.bill_address_id
-          @order.user.save
-        end
-        @order.ship_address_id = @order.user.ship_address_id
-        @order.bill_address_id = @order.user.bill_address_id
-        @order.save
-
         fire_event('spree.checkout.update')
-
         unless @order.next
           flash[:error] = @order.errors[:base].join("\n")
           redirect_to checkout_state_path(@order.state) and return
         end
         if @order.completed?
-
+          if !spree_current_user.credit_card.nil? || spree_current_user.can_bid
             session[:order_id] = nil
             flash.notice = Spree.t(:your_bid_has_been_added_to_the_system)
             flash[:commerce_tracking] = "nothing special"
             redirect_to completion_route
+          else
+            redirect_to add_credit_card_path
+          end
         else
           redirect_to checkout_state_path(@order.state)
         end
@@ -32,4 +24,4 @@ module Spree
       end
     end
   end
-end
+  end
