f92c16f44171ff7fdd68fb9eee9a35609a8e1c57
pyadav <pyadav@enbake.com>
2014-10-31 02:18:57 +0530

    changed the checkout process

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Order.class_eval do
 
   checkout_flow do
-    go_to_state :address, :if => lambda { |order| order.address_not_exist? }
+    go_to_state :address
     go_to_state :payment, :if => lambda { |order| order.payment_required? }
     go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
     go_to_state :complete
@@ -20,7 +20,7 @@ Spree::Order.class_eval do
 
   def display_total_bid_price
     total_bid = 0
-    self.line_items.each do |obj| 
+    self.line_items.each do |obj|
       total_bid = total_bid + (obj.bid_price*obj.quantity) unless obj.bid_price.nil?
     end
     Spree::Money.new(total_bid, { currency: 'PLN' })
@@ -30,11 +30,6 @@ Spree::Order.class_eval do
     Spree::PaymentMethod.select{ |pm| pm.name.downcase =~ /paypal/}.first
   end
 
-  def address_not_exist?
-    # loggedin -> address false
-    #loggedin => no address true
-    # not loggedin => true
-    self.user.present? && self.user.user_address.nil? ? true : false
-  end
+
 
 end
