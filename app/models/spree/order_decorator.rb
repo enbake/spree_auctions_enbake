87b112a8cf04688f87618274b17d0fe86809d938
Mayank <mmishra@enbake.com>
2014-11-03 15:41:43 +0530

    Changed product status once winner paid

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,11 +1,23 @@
 Spree::Order.class_eval do
 
   checkout_flow do
+
     go_to_state :address
     go_to_state :payment, :if => lambda { |order| order.payment_required? }
     go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
     go_to_state :complete
   end
+
+  after_update :change_product_status
+
+  def change_product_status
+    if self.payment_state == "paid"
+      product = self.line_items.first.variant.product
+      product.status = 'closed'
+      product.save
+    end
+  end
+ 
 
   # If true, causes the payment step to happen during the checkout process
   def payment_required?
