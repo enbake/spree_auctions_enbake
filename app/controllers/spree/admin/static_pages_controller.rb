4652e0f23e6ff469194b66b5915a20e9531a1cd8
Mayank <mmishra@enbake.com>
2014-11-10 12:34:42 +0530

    Added translation for faq

--- app/controllers/spree/admin/static_pages_controller.rb
+++ app/controllers/spree/admin/static_pages_controller.rb
@@ -9,9 +9,48 @@ class Spree::Admin::StaticPagesController < Spree::Admin::BaseController
     @static_page = StaticContentPage.new
   end
 
-  def create 
-    StaticContentPage.create!(params[:static_content_page])
-    flash[:notice] = "Topic successfully created"
-    redirect_to '/admin'
+
+
+   def create
+     # params[:category][:url] =  params[:category][:url].split("//").last
+     static_page = StaticContentPage.create!(params[:static_content_page])
+     if params[:lang] == "pl"
+       static_page.translations.where(locale: "en").first.update_attribute(:title=> "",:description => "") if static_page.translations.where(locale: "en").first
+       translation =  static_page.translations.create(locale: params[:lang], title: params[:static_content_page][:title],:description => params[:static_content_page][:description])
+     end
+     flash[:notice] = "Page created successfully"
+     redirect_to  admin_static_pages_url
+   end
+
+   def edit
+     @static_page = StaticContentPage.find(params[:id])
+   end
+
+  def update
+
+    @static_page = StaticContentPage.find params[:id]
+    if @static_page.update_attributes(params[:static_content_page])
+      if params[:lang] == 'pl'
+        translation =  @static_page.translations.find_or_initialize_by_locale(params[:lang])
+        @static_page.translations.where(locale: "en").first.update_attributes(:title=> params[:english_title],
+                                                                              :description => params[:english_description])
+        translation.update_attributes(:title => params[:static_content_page][:title],:description => params[:static_content_page][:description])
+      end
+      flash[:notice] = "Static page updated successfully"
+    else
+      flash[:error] = "Error occured in updating page"
+    end
+    redirect_to admin_static_pages_url
   end
+
+   def destroy
+     @static_page = StaticContentPage.find params[:id]
+     if @static_page.destroy
+       flash[:notice] = "Page deleted successfully"
+     else
+       flash[:error] = "Error occurred while deleting page"
+     end
+     redirect_to admin_static_pages_url
+   end
+
 end
--- app/models/static_content_page.rb
+++ app/models/static_content_page.rb
@@ -1,3 +1,5 @@
 class StaticContentPage < ActiveRecord::Base
   attr_accessible :description, :title
+
+  translates :description, :title
 end
