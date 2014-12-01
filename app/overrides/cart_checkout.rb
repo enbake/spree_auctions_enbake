69ad364de10be01b34ed91af331218139d0e0863
dk-enbake <dkumar@enbake.com>
2014-12-01 13:04:21 +0530

    detail page integration change

--- app/overrides/cart_checkout.rb
+++ app/overrides/cart_checkout.rb
@@ -3,7 +3,9 @@ Deface::Override.new(
     :replace => "div.container",
     :text  => "<div class='container'>
        <% @body_id = 'cart' %>
-  
+  <div class='row margin-bottom-40'>
+    <div class='col-md-12 col-sm-12'>
+        <div class='panel-group checkout-page accordion scrollable' id='checkout-page'>  
         <h1><%= Spree.t(:shopping_cart) %></h1>
   
         <% if @order.line_items.empty? %>
@@ -13,28 +15,25 @@ Deface::Override.new(
       <p><%= link_to Spree.t(:continue_shopping), products_path, :class => 'button continue' %></p>
     </div>
   <% else %>
-    <div data-hook='outside_cart_form'>
+   <div id='confirm-content' class='panel-collapse collapse in' aria-expanded='true'>
+            <div class='panel-body row'>
+              <div class='col-md-12 clearfix'>
+                <div class='table-wrapper-responsive'>
       <%= form_for @order, :url => update_cart_path, :html => {:id => 'update-cart'} do |order_form| %>
-        <div data-hook='inside_cart_form'>
-      
-          <div data-hook='cart_items'>
+        
             <%= render :partial => 'form', :locals => { :order_form => order_form } %>
-          </div>
-     
-  
-
-  
-          
-            <%= button_tag :class => 'button checkout primary', :id => 'checkout-link', :name => 'checkout' do %>
+          <br>
+            <%= button_tag :class => 'btn btn-primary pull-left', :id => 'checkout-link', :name => 'checkout' do %>
               <%= Spree.t(:checkout) %>
             <% end %>
-        </div>
       <% end %>
     </div>
-  
-   
-  
+          </div>
+        </div>
+        </div>
   <% end %>
+  </div>
+    </div>
+  </div>
 </div>",
     :name => "cart_checkout")
-
--- app/overrides/checkout_form.rb
+++ app/overrides/checkout_form.rb
@@ -1,16 +1,16 @@
 Deface::Override.new(
    :virtual_path  => "spree/orders/_form",
    :replace => "table#cart-detail",
-   :text => "<table id='cart-detail' data-hook>
-  <thead>
-    <tr data-hook='cart_items_headers'>
-      <th class='cart-item-description-header' colspan='2'><%= Spree.t(:item) %></th>
-      <th class='cart-item-price-header'><%= Spree.t(:total_price) %></th>
-      <th class='cart-item-delete-header'></th>
+   :text => "<table>
+  <tbody>
+    <tr>
+      <th class='checkout-image'>Image</th>
+      <th class='checkout-description' style='padding-right: 0px;padding-left: 20px;'>Description</th>
+      <th class='checkout-price'>Total Price</th>
     </tr>
-  </thead>
-  <tbody id='line_items' data-hook>
-      <%= render :partial => 'line_item', :locals => { :variant => @order.line_items.first.variant, :line_item => @order.line_items.first, :item_form => @order.line_items.first } %>
+ 
+    <%= render :partial => 'line_item', :locals => { :variant => @order.line_items.first.variant, :line_item => @order.line_items.first, :item_form => @order.line_items.first } %>
   </tbody>
   <%= render 'spree/orders/adjustments' unless @order.adjustments.eligible.blank? %> </table>",
   :name => "checkout_form")
+
